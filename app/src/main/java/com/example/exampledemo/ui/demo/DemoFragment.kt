7e8e7b6f9a68e8edf2906d46d43d54a4858a7457
hailm <haileminhit@gmail.com>
2020-02-25 23:40:44 +0700

    add popup menu demo

--- app/src/main/java/com/example/exampledemo/ui/demo/DemoFragment.kt
+++ app/src/main/java/com/example/exampledemo/ui/demo/DemoFragment.kt
@@ -1,12 +1,27 @@
 package com.example.exampledemo.ui.demo
 
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.PopupWindow
+import androidx.core.widget.PopupWindowCompat
+import androidx.lifecycle.Observer
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.exampledemo.R
 import com.example.exampledemo.app.BaseFragment
 import com.example.exampledemo.databinding.FragmentDemoBinding
+import com.example.exampledemo.databinding.PopupListDataBinding
+import com.example.exampledemo.model.Country
+import com.example.exampledemo.ui.demo.popupwindow.PopupWindowAdapter
 import kotlinx.android.synthetic.main.fragment_demo.*
 
 class DemoFragment : BaseFragment<FragmentDemoBinding, DemoViewModel>() {
+
+    private var listCountries = ArrayList<Country>()
+
+
     override fun getLayoutID(): Int {
         return R.layout.fragment_demo
     }
@@ -19,7 +34,7 @@ class DemoFragment : BaseFragment<FragmentDemoBinding, DemoViewModel>() {
         mBinding.viewModel = mViewModel
 
         btnCanvas.setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.action_demoFragment_to_drawFragment)
+            Navigation.findNavController(it).navigate(R.id.action_demoFragment_to_chatFragment)
         }
 
         btnCountry.setOnClickListener {
@@ -28,6 +43,40 @@ class DemoFragment : BaseFragment<FragmentDemoBinding, DemoViewModel>() {
 
         btnDemo.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.action_demoFragment_to_eggTimerFragment2)
+        }
+
+        context?.let {
+            mViewModel.loadData(it)
+        }
+
+        mViewModel.countriesLiveData.observe(this, Observer { list ->
+            list.let {
+                this.listCountries = list
+            }
+        })
+
+        btnPopupWindow.setOnClickListener {
+            val binding = PopupListDataBinding.inflate(LayoutInflater.from(it.context))
+            val popup = PopupWindow(
+                binding.root,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                it.context.resources.displayMetrics.densityDpi * 500 / 160,
+                true
+            )
+            val rcvCountry = binding.rcvCountry
+            val popupWindowAdapter = PopupWindowAdapter(listCountries)
+            val manager =
+                LinearLayoutManager(it.context, LinearLayoutManager.VERTICAL, false)
+            val dividerItemDecoration =
+                DividerItemDecoration(it.context, manager.orientation)
+            rcvCountry.apply {
+                layoutManager = manager
+                addItemDecoration(dividerItemDecoration)
+                adapter = popupWindowAdapter
+            }
+
+            popup.isOutsideTouchable = true
+            PopupWindowCompat.showAsDropDown(popup, it, 0, 0, Gravity.CENTER)
         }
     }
 }
---
+++ app/src/main/java/com/example/exampledemo/ui/demo/chat/ChatViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.exampledemo.ui.demo.chat
+
+import com.example.exampledemo.app.BaseViewModel
+
+class ChatViewModel : BaseViewModel()
